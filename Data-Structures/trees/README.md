034a6f1ca10b9054136c8b74e3f18f2c8b86f5c6
odai1990 <odai22odai78@gmail.com>
2021-06-26 23:06:04 +0300

    ch15

---
+++ Data-Structures/trees/README.md
@@ -0,0 +1,26 @@
+# Trees
+
+- Make atree node with it's methods
+
+
+## Challenge
+
+
+- Creating a tree that can be traversed with 3 different methods and adding nodes using binary search.
+
+## Approach & Efficiency
+
+
+BinaryTree: they will return the tree in this order:
+
+- pre_order==> root-->left-->right
+- in_order==> left-->root-->right
+- post_order==> left-->right-->root 
+BinarySearchTree:
+- add--> accepts a value, and adds a new node with that value in the correct location in the binary search tree.
+- contains--> accepts a value, and returns a boolean indicating whether or not the value is in the tree at least once. 
+
+
+## API
+
+- my approach was a recursion function.
---
+++ Data-Structures/trees/trees/trees.py
@@ -0,0 +1,99 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+    
+
+    def in_order(self, node=None, results=None):
+      node = node or self.root
+
+      results = results or []
+      if node:
+        if node.left:
+          self.in_order(node.left, results)
+        
+        results.append(node.value)
+
+        if node.right:
+          self.in_order(node.right, results)
+
+      return results
+
+
+    def pre_order(self, node=None, results=None):
+      node = node or self.root
+
+      results = results or []
+
+      if node:
+        results.append(node.value)
+
+        if node.left:
+          self.pre_order(node.left, results)
+
+        if node.right:
+          self.pre_order(node.right, results)
+
+      return results
+
+    def post_order(self, node=None, results=None):
+
+      node = node or self.root
+
+      results = results or []
+      if node:
+        if node.left:
+          self.post_order(node.left, results)
+
+        if node.right:
+          self.post_order(node.right, results)
+        
+        results.append(node.value)
+
+      return results
+
+
+class BinarySearchTree(BinaryTree):
+
+  def add(self, value, root=None):
+    root = root or self.root
+
+    node = Node(value)
+
+    if not self.root:
+      self.root = node
+      return
+    
+    if value < root.value:
+      if root.left:
+        self.add(value, root.left)
+      else:
+        root.left = node
+    else:
+      if root.right:
+        self.add(value, root.left)
+      else:
+       root.right = node
+    
+
+  def contains(self, value, current=None):
+      current = current or self.root
+
+      if not self.root or value == None:
+        print('no')
+        return False
+    
+      if current.value == value:
+        print('yes')
+        return True
+      elif current.value < value:
+        return self.contains(value, current.left)
+      else:
+        return self.contains(value, current.right)
+   
